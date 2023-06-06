7624ec80c5ca8505242d87607ac5708fb9988cb8
Toni Gjaja <79863380+toni-gjaja@users.noreply.github.com>
2023-06-06 10:03:26 +0200

    Controllers and html pages

---
+++ src/main/java/com/jwp/webshop/controller/HomeController.java
@@ -0,0 +1,14 @@
+package com.jwp.webshop.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("app")
+public class HomeController {
+
+
+
+
+
+}
---
+++ src/main/java/com/jwp/webshop/controller/LoginController.java
@@ -0,0 +1,9 @@
+package com.jwp.webshop.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("login")
+public class LoginController {
+}
