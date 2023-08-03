f584bf0457c9ffe9bd9c087b0bb7ac4eeaf98619
Toni Gjaja <79863380+toni-gjaja@users.noreply.github.com>
2023-08-03 23:31:46 +0200

    Connection set for local PostgreSQL database

--- src/main/java/hr/webshop/WebShopApplication.java
+++ src/main/java/hr/webshop/WebShopApplication.java
@@ -7,7 +7,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class WebShopApplication {
 
     public static void main(String[] args) {
+
         SpringApplication.run(WebShopApplication.class, args);
+
     }
 
 }
